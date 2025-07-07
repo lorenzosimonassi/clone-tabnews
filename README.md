742589ec516dc5e64abaa92b56866cca17b022f7
lorenzosimonassi <lorenzosimonassimoura@gmail.com>
2025-07-07 16:46:25 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Dia 2 do curso.dev. Implementando o https://www.tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,50 @@
-
 function Home() {
   return (
-    <div style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        minHeight: '80vh',
-        width: 'full',
-        padding: '20px',
-        boxSizing: 'border-box',
-        fontFamily: 'helvetica',
-    }}>
-
-      <div style={{
-        textAlign: 'center',
-        padding: '2rem',
-        backgroundColor: '#f0f0f0',
-        borderRadius: '10px',
-        maxWidth: '800px',
-        margin: '0 auto',
-        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
-      }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "80vh",
+        width: "full",
+        padding: "20px",
+        boxSizing: "border-box",
+        fontFamily: "helvetica",
+      }}
+    >
+      <div
+        style={{
+          textAlign: "center",
+          padding: "2rem",
+          backgroundColor: "#f0f0f0",
+          borderRadius: "10px",
+          maxWidth: "800px",
+          margin: "0 auto",
+          boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+        }}
+      >
         <h1>João ⚔️ 'Sir David Beckham', Here We Go!</h1>
-        <h3>É do FUTEBOL REAL! João e Futebol Real chegam num acordo de  21.500.000 milhões de euros em um contrato de 4 anos com salário de 70mil euros mensalmente.</h3>
-        
-        <video width="700" height="500"  loop autoPlay muted reverse>
-            <source src="/images/joaovideo.mp4" type="video/mp4" />
+        <h3>
+          É do FUTEBOL REAL! João e Futebol Real chegam num acordo de 21.500.000
+          milhões de euros em um contrato de 4 anos com salário de 70mil euros
+          mensalmente.
+        </h3>
+
+        <video width="700" height="500" loop autoPlay muted reverse>
+          <source src="/images/joaovideo.mp4" type="video/mp4" />
         </video>
 
-        <audio autoPlay loop >
-        <source src="/images/obsessao.mp3" type="audio/mpeg"/>
+        <audio autoPlay loop>
+          <source src="/images/obsessao.mp3" type="audio/mpeg" />
         </audio>
 
-        <div style={{ marginTop: '20px', fontSize: '18px', fontWeight:'bold' }}>
-          <p>O mestre da física, o rei do passe preciso, o goleiro com crise de ansiedade, o inimigo das interceptações! </p>
+        <div
+          style={{ marginTop: "20px", fontSize: "18px", fontWeight: "bold" }}
+        >
+          <p>
+            O mestre da física, o rei do passe preciso, o goleiro com crise de
+            ansiedade, o inimigo das interceptações!{" "}
+          </p>
           <p>O 'chef' 🧑‍🍳 que cozinha seus adversários no mental 🧠.</p>
         </div>
       </div>
